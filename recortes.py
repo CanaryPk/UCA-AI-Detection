a2deb5d63defb00e7ab4cc6060699d25a6a6dda2
Santiago Sánchez Loureiro <126728395+CanaryPk@users.noreply.github.com>
2025-07-28 13:29:04 +0200

    Comentarios para mayor claridad.

--- recortes.py
+++ recortes.py
@@ -6,15 +6,19 @@ import cv2
 import os
 from ultralytics import YOLO
 
+# Cargamos el modelo YOLOv11.
 model = YOLO("yolo11n.pt")
 clases = model.names
 
+# Ruta del video y nombre base del video.
 video_path = "../001_DAY_TXT_CLEAR_BRAKECHECK/1.mp4"
 nombre_video = os.path.splitext(os.path.basename(video_path))[0]
 
+# Creamos carpeta raíz para guardar recortes.
 output_root = f"recortes/{nombre_video}"
 os.makedirs(output_root, exist_ok=True)
 
+# Iniciar captura de video.
 cap = cv2.VideoCapture(video_path)
 frame_id = 0
 
@@ -24,7 +28,7 @@ while cap.isOpened():
         break
 
     frame_id += 1
-    frame_sin_anotaciones = frame.copy()
+    frame_sin_anotaciones = frame.copy() # Copia del frame original para no guardar las «bounding boxes».
 
     resultados = model.track(frame, persist=True)
 
@@ -37,14 +41,16 @@ while cap.isOpened():
         for caja, id_track, id_clase, conf in zip(cajas, ids, indices_clase, confidences):
             x1, y1, x2, y2 = map(int, caja)
 
-            # Recorte limpio del vehículo
-            recorte = frame_sin_anotaciones[max(0, y1):max(0, y2), max(0, x1):max(0, x2)]
-            if recorte.size == 0:
+            # Recorte limpio del vehículo.
+            recorte = frame_sin_anotaciones[max(0, y1):max(0, y2), max(0, x1):max(0, x2)] 
+            if recorte.size == 0: # Si el recorte está vacío, ignoramos.
                 continue
 
-            carpeta_id = os.path.join(output_root, f"vehiculo{id_track}")
+            # Guardamos el recorte en la carpeta correspondiente al ID del vehículo.
+            carpeta_id = os.path.join(output_root, f"id{id_track}")
             os.makedirs(carpeta_id, exist_ok=True)
 
+            # Guardamos el recorte con el frame actual como nombre.
             nombre_imagen = f"frame_{frame_id:05}.jpg"
             ruta_guardado = os.path.join(carpeta_id, nombre_imagen)
             cv2.imwrite(ruta_guardado, recorte)
