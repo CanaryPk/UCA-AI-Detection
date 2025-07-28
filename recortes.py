05c4899545f86d777fbfb9aef79dcf43d2cbe9bc
Santiago Sánchez Loureiro <126728395+CanaryPk@users.noreply.github.com>
2025-07-28 13:21:27 +0200

    Add files via upload

--- recortes.py
+++ recortes.py
@@ -1,21 +1,20 @@
+# Primeros pasos para la segmentación de los vehículos en cada frame.
+# El programa guarda en la carpeta "recortes" cada uno de los vehículos del vídeo en forma de carpeta. En cada carpeta se 
+# guardan los recortes del respectivo vehículo en cada uno de los frames del vídeo.
+
 import cv2
 import os
 from ultralytics import YOLO
-from collections import defaultdict
 
-# Cargar el modelo YOLOv11
 model = YOLO("yolo11n.pt")
 clases = model.names
 
-# Ruta del video y nombre base del video
 video_path = "../001_DAY_TXT_CLEAR_BRAKECHECK/1.mp4"
 nombre_video = os.path.splitext(os.path.basename(video_path))[0]
 
-# Crear carpeta raíz para guardar recortes
 output_root = f"recortes/{nombre_video}"
 os.makedirs(output_root, exist_ok=True)
 
-# Iniciar captura de video
 cap = cv2.VideoCapture(video_path)
 frame_id = 0
 
@@ -24,8 +23,10 @@ while cap.isOpened():
     if not leido:
         break
 
+    frame_id += 1
+    frame_sin_anotaciones = frame.copy()
+
     resultados = model.track(frame, persist=True)
-    frame_id += 1  # Número de frame actual
 
     if resultados[0].boxes.data is not None:
         cajas = resultados[0].boxes.xyxy.cpu()
@@ -35,33 +36,17 @@ while cap.isOpened():
 
         for caja, id_track, id_clase, conf in zip(cajas, ids, indices_clase, confidences):
             x1, y1, x2, y2 = map(int, caja)
-            nombre_clase = clases[id_clase]
 
-            # Dibujar en el frame
-            cv2.putText(frame, f"ID: {id_track} {nombre_clase}", (x1, y1-10),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 255, 255), 2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-            # Recorte del objeto
-            recorte = frame[max(0, y1):max(0, y2), max(0, x1):max(0, x2)]
+            # Recorte limpio del vehículo
+            recorte = frame_sin_anotaciones[max(0, y1):max(0, y2), max(0, x1):max(0, x2)]
             if recorte.size == 0:
-                continue  # Saltar si el recorte está fuera del frame
+                continue
 
-            # Crear carpeta para este ID si no existe
             carpeta_id = os.path.join(output_root, f"vehiculo{id_track}")
             os.makedirs(carpeta_id, exist_ok=True)
 
-            # Guardar imagen del recorte
             nombre_imagen = f"frame_{frame_id:05}.jpg"
             ruta_guardado = os.path.join(carpeta_id, nombre_imagen)
             cv2.imwrite(ruta_guardado, recorte)
 
-    # Mostrar el frame en tiempo real
-    cv2.imshow("Seguimiento de vehículos con YOLOv11", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Liberar recursos
 cap.release()
-cv2.destroyAllWindows()
