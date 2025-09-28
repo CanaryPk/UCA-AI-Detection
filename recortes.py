ca9be23f452ca1f7e74516a7454aa8d37611a672
Santiago Sánchez Loureiro <126728395+CanaryPk@users.noreply.github.com>
2025-09-28 19:27:58 +0200

    Automatización de recortes.
    Automatizado el recorrido de la carpeta de vídeos. Ahora se generan carpetas para cada vehículo en cada frame, para cada vídeo.

--- recortes.py
+++ recortes.py
@@ -9,45 +9,47 @@ from ultralytics import YOLO
 # Load our model with YOLOv11.
 model = YOLO("yolo11n.pt")
 
-# Path to the original video, video name and folder.
-video_path = "../videos/001_DAY_TXT_CLEAR_BRAKECHECK/1.mp4"
-folder = os.path.split(os.path.dirname(video_path))[1] # 001_DAY_TXT_CLEAR_BRAKECHECK
-video_name = os.path.basename(video_path) # 1.mp4
+base_directory = "../videos"
+for base_folder in os.listdir(base_directory):
+    if base_folder != "recortes":
+        # Path to the original video, video name and folder.
+        video_path = os.path.join(base_directory, base_folder)
+        video_name = os.listdir(video_path)[0]
+        video_path = os.path.join(video_path, video_name)
 
-# Create root directory to save car snippets.
-output_path = f"../videos/recortes/{folder}"
-os.makedirs(output_path, exist_ok=True)
+        # Create root directory to save car snippets.
+        output_path = f"../videos/recortes/{base_folder}"
+        os.makedirs(output_path, exist_ok=True)
 
-# Start video capture.
-cap = cv2.VideoCapture(video_path)
-frame_id = 0
+        # Start video capture.
+        cap = cv2.VideoCapture(video_path)
+        frame_id = 0
 
-read, frame = cap.read()
-while cap.isOpened() and read:
-    print("vuelta ", frame_id)
-    frame_id += 1
-    clean_frame = frame.copy() # Copy of the original frame, without bounding boxes.
+        read, frame = cap.read()
+        while cap.isOpened() and read:
+            print("Vuelta: ", frame_id)
+            print("Ruta: ", output_path)
+            frame_id += 1
+            clean_frame = frame.copy() # Copy of the original frame, without bounding boxes.
 
-    results = model.track(frame, persist=True)
-    if results and results[0].boxes is not None:
-        print("results no es vacio ni none")
-        boxes = results[0].boxes.xyxy.cpu()
-        ids = results[0].boxes.id.int().cpu().tolist()
+            results = model.track(frame, persist=True)
+            if results and results[0].boxes is not None and len(results[0].boxes) > 0 and results[0].boxes.id is not None:
+                boxes = results[0].boxes.xyxy.cpu()
+                ids = results[0].boxes.id.int().cpu().tolist()
 
-        for box, id_track in zip(boxes, ids):
-            x1, y1, x2, y2 = map(int, box)
+                for box, id_track in zip(boxes, ids):
+                    x1, y1, x2, y2 = map(int, box)
 
-            # Clean snippet of the vehicle
-            snippet = clean_frame[max(0, y1):max(0, y2), max(0, x1):max(0, x2)]
-            if snippet.size != 0: # If the current clipping is NOT empty...
-                # Save the snippet in the folder corresponding to its vehicle ID.
-                folder_id = os.path.join(output_path, f"id{id_track}")
-                os.makedirs(folder_id, exist_ok=True)
+                    # Clean snippet of the vehicle
+                    snippet = clean_frame[max(0, y1):max(0, y2), max(0, x1):max(0, x2)]
+                    if snippet.size != 0: # If the current clipping is NOT empty...
+                        # Save the snippet in the folder corresponding to its vehicle ID.
+                        folder_id = os.path.join(output_path, f"id{id_track}")
+                        os.makedirs(folder_id, exist_ok=True)
 
-                # Guardamos el recorte con el frame actual como nombre.
-                img_name = f"frame_{frame_id}.jpg"
-                save_path = os.path.join(folder_id, img_name)
-                cv2.imwrite(save_path, snippet)
-    read, frame = cap.read()
-
-cap.release()
+                        # Guardamos el recorte con el frame actual como nombre.
+                        img_name = f"frame_{frame_id}.jpg"
+                        save_path = os.path.join(folder_id, img_name)
+                        cv2.imwrite(save_path, snippet)
+            read, frame = cap.read()
+        cap.release()
